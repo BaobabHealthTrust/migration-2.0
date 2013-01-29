b88d3ed5d859d4c712f49b7b197f47f113042b5e
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-01-29 12:22:50 +0200

    defining model relationships

--- app/models/visit_encounter.rb
+++ app/models/visit_encounter.rb
@@ -2,18 +2,18 @@ class VisitEncounter < ActiveRecord::Base
   set_table_name :visit_encounters
   set_primary_key :id
   has_many :first_visit_encounters, :foreign_key => :visit_encounter_id, 
-    :dependent => :destroy, :class => 'FirstVisitEncounter'
+    :dependent => :destroy, :class_name => 'FirstVisitEncounter'
   has_many :give_drugs_encounters, :foreign_key => :visit_encounter_id, 
-    :dependent => :destroy, :class => 'GiveDrugsEncounter'
+    :dependent => :destroy, :class_name => 'GiveDrugsEncounter'
   has_many :hiv_staging_encounters, :foreign_key => :visit_encounter_id, 
-    :dependent => :destroy, :class => 'HivStagingEncounter'
+    :dependent => :destroy, :class_name => 'HivStagingEncounter'
   has_many :outcome_encounters, :foreign_key => :visit_encounter_id, 
-    :dependent => :destroy, :class => 'OutcomeEncounter'
+    :dependent => :destroy, :class_name => 'OutcomeEncounter'
   has_many :pre_art_encounters, :foreign_key => :visit_encounter_id, 
-    :dependent => :destroy, :class => 'PreArtVisitEncounter'
+    :dependent => :destroy, :class_name => 'PreArtVisitEncounter'
   has_many :art_encounters, :foreign_key => :visit_encounter_id, 
-    :dependent => :destroy, :class => 'ArtVisitEncounter'
+    :dependent => :destroy, :class_name => 'ArtVisitEncounter'
   has_many :vitals_encounters, :foreign_key => :visit_encounter_id, 
-    :dependent => :destroy, :class => 'VitalsEncounter'
+    :dependent => :destroy, :class_name => 'VitalsEncounter'
 
 end
