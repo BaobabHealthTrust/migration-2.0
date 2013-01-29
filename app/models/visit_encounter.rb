e35d9b35d7508c417bf816a1e0aaa9549d3bff48
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-01-29 10:33:34 +0200

    adding new models

--- app/models/visit_encounter.rb
+++ app/models/visit_encounter.rb
@@ -1,2 +1,6 @@
 class VisitEncounter < ActiveRecord::Base
+  set_table_name :visit_encounter
+  set_primary_key :id
+
+  has_many :encounters, :foreign_key => :encounter_id, :dependent => :destroy
 end
