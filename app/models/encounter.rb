3ae79be70db98a091b39a60ff4918716f44b4d65
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-01-29 13:01:17 +0200

    adding model relationships

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -2,6 +2,7 @@ class Encounter < ActiveRecord::Base
 	set_table_name :encounter
 	set_primary_key :encounter_id
 	has_many :observations, :foreign_key => :encounter_id
+	has_many :orders, :foreign_key => :encounter_id
 	has_one :encounter_type, :foreign_key => :encounter_type_id
 	belongs_to :user, :foreign_key => :user_id
 	belongs_to :patient, :foreign_key => :patient_id
