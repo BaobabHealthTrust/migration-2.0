40749fae5690670eb3be4d4f6e8c497ae9f6e52a
Deliwe <dbnkhoma@gmail.com>
2013-05-20 10:37:58 +0200

    added a relative_id field

--- db/migrate/20130128144247_create_guardians.rb
+++ db/migrate/20130128144247_create_guardians.rb
@@ -9,6 +9,7 @@ ActiveRecord::Base.connection.execute <<EOF
 create table `guardians`(
 `id` int not null auto_increment primary key,
 `patient_id` int not null,
+`relative_id` int not null,
 `name` varchar(255),
 `relationship` varchar(255),
 `family_name` varchar(255),
