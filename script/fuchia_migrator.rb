bbe99915aea11e4a9b804245f02ff4aa5b1d6730
timmtonga <timmtonga@gmail.com>
2013-07-15 13:26:58 +0200

    fixed bugs in fuchia migrator script

--- script/fuchia_migrator.rb
+++ script/fuchia_migrator.rb
@@ -5,14 +5,12 @@ $enc_id = 1
 $outcome_id = 1
 
 def start
-
+  count = 1
   patients =  $mysql_conn.query("SELECT * FROM tbpatient LIMIT 5")
   patients.each do |row|
-
+    puts "Working with patient #{count}"
     create_patient(row)
     create_patient_outcomes(row)
-
-
 
     followups = $mysql_conn.query("SELECT * FROM tbfollowup WHERE FdxReferencePatient = #{row[0]} AND FdxReferenceProgram
                                    IN (167,168,169,379,380,381) ORDER BY FddVisit ASC")
@@ -26,30 +24,30 @@ def start
       end
 
       if pre_art_patient(row[0], followup_visit[9])
-        #create_pre_art_visit_enc()
+        create_pre_art_visit_enc(row, followup_visit)
       else
         create_art_visit_enc(row, followup_visit)
       end
 
       create_vitals_enc(followup_visit)
       create_hiv_recp_enc(followup_visit)
-      #create_give_drugs_encounters()
+      create_give_drugs_encounters(row, followup_visit)
 
     end
-
+    count +=1
   end
 
 end
 
 def create_patient(patient_array)
-
+  puts "Creating patient"
   patient = PatientRecord.new()
   patient.patient_id = patient_array[0]
   patient.given_name = patient_array[9].split(" ")[0]
   patient.middle_name = nil
   patient.family_name = patient_array[9].split(" ")[1] rescue nil
   patient.gender = patient_array[10] == "0" ? "Male" : "Female"
-  patient.dob = patient_array[11].nil? ? get_dob_esitmated(patient_array[12],  patient_array[18]) : patient_array[11]
+  patient.dob = get_dob(patient_array)
   patient.dob_estimated = patient_array[12].nil? ? false : true
   patient.current_address = $mysql_conn.query("SELECT FdsLookup FROM tbreference WHERE FdxReference = #{patient_array[3]}").fetch_row[0]
   patient.occupation= $mysql_conn.query("SELECT FdsLookup FROM tbreference WHERE FdxReference = #{patient_array[5]}").fetch_row[0]
@@ -103,7 +101,7 @@ def create_vitals_enc(follow_up_enc)
 end
 
 def create_hiv_recp_enc(follow_up_enc)
-
+  puts "creating hiv reception encounter"
   enc = HivReceptionEncounter.new()
   enc.patient_id = follow_up_enc[3]
   enc.visit_encounter_id = check_for_visit_date(follow_up_enc[3], follow_up_enc[9])
@@ -120,6 +118,7 @@ end
 
 def create_hiv_first_visit_enc(patient_data, visit_data)
 
+  puts "creating hiv first visit encounter"
   transfer_in = patient_data[14] == "-1" ? true : false
   enc = FirstVisitEncounter.new()
   enc.patient_id = patient_data[0]
@@ -193,6 +192,7 @@ end
 
 def create_hiv_staging_enc(patient_data)
 
+  puts "creating hiv staging encounter"
   diagnosis = $mysql_conn.query("SELECT * FROM tbpatientdiagnosis WHERE FdxReferencePatient = #{patient_data[0]}")
 
   if diagnosis.num_rows > 0
@@ -208,7 +208,7 @@ def create_hiv_staging_enc(patient_data)
     enc.who_stage = "WHO stage" + patient_data[38].to_s rescue nil
     enc.reason_for_starting_art = "Unknown"
     diagnosis.each do |ob|
-      self.repeated_obs(enc, ob[4])
+      self.repeated_obs_staging(enc, ob[4])
     end
 
     enc.save
@@ -219,6 +219,7 @@ end
 
 def create_art_visit_enc(patient_data, visit_data)
 
+  puts "Creating art visit encounter"
   enc = ArtVisitEncounter.new()
   enc.patient_id = patient_data[0]
   enc.visit_encounter_id = self.check_for_visit_date(patient_data[0],visit_data[9])
@@ -230,10 +231,63 @@ def create_art_visit_enc(patient_data, visit_data)
   enc.tb_status = check_if_patient_has_tb(patient_data[0], visit_data[9], visit_data[13])
   enc.prescribe_cpt = $mysql_conn.query("SELECT * FROM tbfollowupdrug WHERE FdxReferenceFollowUp = #{visit_data[0]}
                                           AND FdxReferenceDrug IN (56,148)").num_rows > 0 ? "Yes" : "No"
+  enc.prescribe_arv = $mysql_conn.query("SELECT * FROM tbfollowupdrug WHERE FdxReferenceFollowUp = #{visit_data[0]}
+                                          AND FdxReferenceDrug NOT IN (56,148)").num_rows > 0 ? "Yes" : "No"
+  drugs = $mysql_conn.query("SELECT * FROM tbfollowupdrug WHERE FdxReferenceFollowUp = #{visit_data[0]}")
 
-  #enc.continue_art = "Yes"
-  #enc.continue_treatment_at_clinic =
+  drugs.each do |dispensed_drug|
 
+    self.drug_induced_symptom(enc, dispensed_drug[5] )
+    self.drug_induced_symptom(enc, dispensed_drug[6] )
+
+  end
+
+  last_visit = $mysql_conn.query("SELECT * FROM tbfollowupdrug WHERE FdxReferenceFollowUp =
+                                  (SELECT FdxReference FROM tbfollowup WHERE FdxReferencePatient = #{patient_data[0]} AND
+                                  FddVisit < '#{visit_data[9]}' ORDER BY FddVisit DESC LIMIT 1)")
+
+  i = 1
+
+  last_visit.each do |dispensed|
+
+    drug_detail = $mysql_conn.query("SELECT * FROM drug_map WHERE FdxReference = #{dispensed[4]}").fetch_row
+
+    case i
+      when 1
+
+        enc.drug_name_brought_to_clinic1 = drug_detail[5] rescue nil
+        enc.drug_quantity_brought_to_clinic1 =  get_brought(amount_dispensed(drug_detail[10], (visit_data[10].to_date - visit_data[9].to_date).to_i),   visit_data[34]) rescue nil
+
+        enc.drug_left_at_home1 = nil
+      when 2
+        enc.drug_name_brought_to_clinic2 = drug_detail[5] rescue nil
+        enc.drug_quantity_brought_to_clinic2 =  get_brought(amount_dispensed(drug_detail[10], (visit_data[10].to_date - visit_data[9].to_date).to_i),   visit_data[34]) rescue nil
+
+        enc.drug_left_at_home1 = nil
+      when 3
+        enc.drug_name_brought_to_clinic3 = drug_detail[5] rescue nil
+        enc.drug_quantity_brought_to_clinic3 = get_brought(amount_dispensed(drug_detail[10], (visit_data[10].to_date - visit_data[9].to_date).to_i),   visit_data[34]) rescue nil
+
+        enc.drug_left_at_home1 = nil
+      when 4
+        enc.drug_name_brought_to_clinic4 = drug_detail[5] rescue nil
+        enc.drug_quantity_brought_to_clinic4 = get_brought(amount_dispensed(drug_detail[10], (visit_data[10].to_date - visit_data[9].to_date).to_i),   visit_data[34]) rescue nil
+
+        enc.drug_left_at_home1 = nil
+    end
+    i +=1
+  end
+
+
+  if ((enc.prescribe_arv == "Yes") || (enc.prescribe_cpt == "Yes")) && (!visit_data[10].blank?)
+    enc.continue_art = "Yes"
+    enc.continue_treatment_at_clinic = "Yes"
+  else
+    enc.continue_art = "No"
+    enc.continue_treatment_at_clinic = "No"
+  end
+
+  create_patient_outcome(patient_data[0], "On ART", visit_data[9])
 
   diagnosis = $mysql_conn.query("SELECT * FROM tbfollowupdiagnosis WHERE FdxReferenceFollowUp = #{visit_data[0]}")
   diagnosis.each do |ob|
@@ -241,6 +295,109 @@ def create_art_visit_enc(patient_data, visit_data)
   end
 
   enc.save()
+  $enc_id +=1
+
+end
+
+def create_pre_art_visit_enc(patient_details, visit_data)
+
+  puts "creating pre-art visit encounter"
+  enc = PreArtVisitEncounter.new()
+  enc.patient_id = patient_details[0]
+  enc.location = "Thyolo district hospital"
+  enc.visit_encounter_id = self.check_for_visit_date(patient_details[0], visit_data[9])
+  enc.old_enc_id = $enc_id
+  enc.date_created = visit_data[1]
+  enc.encounter_datetime = visit_data[9]
+  enc.creator = 1
+  enc.prescription_duration = (visit_data[10].to_date - visit_data[9].to_date).to_i
+  enc.tb_status = check_if_patient_has_tb(patient_details[0], visit_data[9], visit_data[13])
+  enc.prescribe_cpt = $mysql_conn.query("SELECT * FROM tbfollowupdrug WHERE FdxReferenceFollowUp = #{visit_data[0]}
+                                          AND FdxReferenceDrug IN (56,148)").num_rows > 0 ? "Yes" : "No"
+  enc.prescribe_ipt= $mysql_conn.query("SELECT * FROM tbfollowupdrug WHERE FdxReferenceFollowUp = #{visit_data[0]}
+                                          AND FdxReferenceDrug IN (59,151)").num_rows > 0 ? "Yes" : "No"
+  diagnosis = $mysql_conn.query("SELECT * FROM tbfollowupdiagnosis WHERE FdxReferenceFollowUp = #{visit_data[0]}")
+  diagnosis.each do |ob|
+    self.repeated_obs(enc, ob[4])
+  end
+
+  drugs = $mysql_conn.query("SELECT FdxReferenceIntolerance02, FdxReferenceIntolerance01 FROM tbfollowupdrug WHERE FdxReferenceFollowUp = #{visit_data[0]}")
+
+  drugs.each do |dispensed_drug|
+
+
+    self.drug_induced_symptom(enc, dispensed_drug[1])
+    self.drug_induced_symptom(enc, dispensed_drug[0])
+
+
+  end
+  create_patient_outcome(patient_details[0], "Pre-ART (Continue)", visit_data[9])
+  enc.save
+  $enc_id += 1
+end
+
+def create_give_drugs_encounters(patient_data, visit_data)
+
+  puts "Creating give drugs encounter, reference #{visit_data[0]}"
+  enc = GiveDrugsEncounter.new()
+  enc.patient_id = patient_data[0]
+  enc.visit_encounter_id = self.check_for_visit_date(patient_data[0],visit_data[9])
+  enc.old_enc_id = $enc_id
+  enc.location = "Thyolo district hospital"
+  enc.date_created = visit_data[1]
+  enc.encounter_datetime = visit_data[9]
+  enc.prescription_duration =  (visit_data[10].to_date - visit_data[9].to_date).to_i rescue nil
+  enc.appointment_date = visit_data[10]
+  enc.creator = 1
+
+  drugs = $mysql_conn.query("SELECT * FROM tbfollowupdrug WHERE FdxReferenceFollowUp = #{visit_data[0]}")
+  count = 1
+  drugs.each do |drug|
+
+    case count
+      when 1
+        enc.pres_drug_name1 = drug_detail[5] rescue nil
+        enc.pres_dosage1 = drug[10]
+        enc.pres_frequency1= "Evening-Noon-Night-Morning"
+        enc.dispensed_quantity1 = amount_dispensed(drug[10], (visit_data[10].to_date - visit_data[9].to_date).to_i ) rescue nil
+        enc.dispensed_drug_name1 = enc.pres_drug_name1
+
+
+      when 2
+        enc.pres_drug_name2 = drug_detail[5] rescue nil
+        enc.pres_dosage2 = drug[10]
+        enc.pres_frequency2= "Evening-Noon-Night-Morning"
+        enc.dispensed_quantity2 = amount_dispensed(drug[10], (visit_data[10].to_date - visit_data[9].to_date).to_i ) rescue nil
+        enc.dispensed_drug_name2 = enc.pres_drug_name1
+
+      when 3
+        enc.pres_drug_name3 = drug_detail[5] rescue nil
+        enc.pres_dosage3 = drug[10]
+        enc.pres_frequency3= "Evening-Noon-Night-Morning"
+        enc.dispensed_quantity3 = amount_dispensed(drug[10], (visit_data[10].to_date - visit_data[9].to_date).to_i ) rescue nil
+        enc.dispensed_drug_name3 = enc.pres_drug_name1
+
+      when 4
+        enc.pres_drug_name4 = drug_detail[5] rescue nil
+        enc.pres_dosage4 = drug[10]
+        enc.pres_frequency4= "Evening-Noon-Night-Morning"
+        enc.dispensed_quantity4 = amount_dispensed(drug[10], (visit_data[10].to_date - visit_data[9].to_date).to_i ) rescue nil
+        enc.dispensed_drug_name4 = enc.pres_drug_name4
+
+
+      when 5
+        enc.pres_drug_name5 = drug_detail[5] rescue nil
+        enc.pres_dosage5 = drug[10]
+        enc.pres_frequency5= "Evening-Noon-Night-Morning"
+        enc.dispensed_quantity5 = amount_dispensed(drug[10], (visit_data[10].to_date - visit_data[9].to_date).to_i ) rescue nil
+        enc.dispensed_drug_name5 = enc.pres_drug_name5
+
+    end
+
+
+  end
+
+  enc.save
   $enc_id +=1
 
 end
@@ -282,14 +439,50 @@ def check_if_ever_received_art(patient_id)
 
 end
 
-def self.repeated_obs(enc, ob)
+def self.repeated_obs(enc,ob)
+  case $mysql_conn.query("SELECT FdsLookup FROM tbreference WHERE FdxReference = #{ob}").fetch_row.to_s.upcase
+    when 'PANCREATIC AMYLASE (>2-5 x ULN) AND ABDOMINAL PAIN'
+      enc.abdominal_pains = "YES"
+    when 'ANOREXIA'
+      enc.anorexia = "YES"
+    when 'COUGH'
+      enc.cough = "YES"
+    when 'DIARRHOEA'
+      enc.diarrhoea = "YES"
+    when 'FEVER, UNEXPLAINED'
+      enc.fever = "YES"
+    when 'JAUNDICE'
+      enc.jaundice = "YES"
+    when 'LEG PAIN / NUMBNESS'
+      enc.leg_pain_numbness = "YES"
+    when 'VOMIT'
+      enc.vomit = "YES"
+    when 'WEIGHT LOSS <10%'
+      enc.weight_loss = "YES"
+    when 'HEPATITIS'
+      enc.hepatitis = "YES"
+    when 'Anemia 1-2 (Hb 7-9.4)'
+      enc.anaemia = "YES"
+    when 'Anemia 3 (Hb 6.5-6.9)'
+      enc.anaemia = "YES"
+    when 'Anemia 4 (Hb <6.5)'
+      enc.anaemia = "YES"
+    when 'LACTIC ACIDOSIS'
+      enc.lactic_acidosis = "YES"
+    when 'LIPODYSTROPHY'
+      enc.lipodystrophy = "YES"
+    when 'SKIN RASH'
+      enc.skin_rash = "YES"
+    when 'PERIPHERAL NEUROPATHY'
+      enc.peripheral_neuropathy = "YES"
 
-  case $mysql_conn.query("SELECT FdsLookup WHERE FdxReference = #{ob}").fetch_row.to_s.upcase
-    when 'PREGNANT'
-      enc.patient_pregnant = "YES"
-    when 'BREASTFEEDING'
-      enc.patient_breast_feeding = "YES"
-    when 'ABDOMINAL PAIN'
+  end
+end
+def self.repeated_obs_staging(enc, ob)
+
+  case $mysql_conn.query("SELECT FdsLookup FROM tbreference WHERE FdxReference = #{ob}").fetch_row.to_s.upcase
+
+    when 'PANCREATIC AMYLASE (>2-5 x ULN) AND ABDOMINAL PAIN'
       enc.abdominal_pains = "YES"
     when 'ANOREXIA'
       enc.anorexia = "YES"
@@ -431,42 +624,33 @@ def self.repeated_obs(enc, ob)
   end
 end
 
-def self.drug_induced_symptom (enc)
-  if enc.lipodystrophy.upcase == 'YES DRUG INDUCED'
-    enc.drug_induced_lipodystrophy = 'Yes'
-  end
-  if enc.abdominal_pains.upcase == 'YES DRUG INDUCED'
-    enc.drug_induced_abdominal_pains = 'Yes'
-  end
-  if enc.skin_rash.upcase == 'YES DRUG INDUCED'
-    enc.drug_induced_skin_rash = 'Yes'
-  end
-  if enc.anorexia.upcase == 'YES DRUG INDUCED'
-    enc.drug_induced_anorexia = 'Yes'
-  end
-  if enc.diarrhoea.upcase == 'YES DRUG INDUCED'
-    enc.drug_induced_diarrhoea = 'Yes'
-  end
-  if enc.jaundice.upcase == 'YES DRUG INDUCED'
-    enc.drug_induced_jaundice = 'Yes'
-  end
-  if enc.leg_pain_numbness.upcase == 'YES DRUG INDUCED'
-    enc.drug_induced_leg_pain_numbness = 'Yes'
-  end
-  if enc.vomit.upcase == 'YES DRUG INDUCED'
-    enc.drug_induced_vomit = 'Yes'
-  end
-  if enc.peripheral_neuropathy.upcase == 'YES DRUG INDUCED'
-    enc.drug_induced_peripheral_neuropathy = 'Yes'
-  end
-  if enc.hepatitis.upcase == 'YES DRUG INDUCED'
-    enc.drug_induced_hepatitis = 'Yes'
-  end
-  if enc.anaemia.upcase == 'YES DRUG INDUCED'
-    enc.drug_induced_anaemia = 'Yes'
-  end
-  if enc.lactic_acidosis.upcase == 'YES DRUG INDUCED'
-    enc.drug_induced_lactic_acidosis = 'Yes'
+def self.drug_induced_symptom(enc, intolerance)
+
+  unless intolerance.blank?
+    drug_cozed_symp =  $mysql_conn.query("SELECT FdsLookup FROM tbreference WHERE FdxReference = #{intolerance} LIMIT 1").fetch_row
+
+    if ((drug_cozed_symp == "Mild Lipodystrophy") || (drug_cozed_symp == 'Severe Lipodystrophy'))
+      enc.drug_induced_lipodystrophy = 'Yes'
+
+    elsif ((drug_cozed_symp == 'Skin Eruption 1/2') || (drug_cozed_symp == 'Skin Eruption 3/4'))
+      enc.drug_induced_skin_rash = 'Yes'
+
+    elsif ((drug_cozed_symp == 'Diarrhea 1/2') || (drug_cozed_symp == 'Diarrhea 3') || (drug_cozed_symp == 'Diarrhea 4'))
+      enc.drug_induced_diarrhoea = 'Yes'
+
+    elsif (drug_cozed_symp == 'Jaundice')
+      enc.drug_induced_jaundice = 'Yes'
+
+    elsif ((drug_cozed_symp == 'Neuropathy 1-2') ||(drug_cozed_symp == 'Neuropathy 3') || (drug_cozed_symp == 'Neuropathy 4') )
+      enc.drug_induced_peripheral_neuropathy = 'Yes'
+
+    elsif ((drug_cozed_symp == 'Anemia 1-2 (Hb 7-9.4)')|| (drug_cozed_symp == 'Anemia 3 (Hb 6.5-6.9)') || (drug_cozed_symp == 'Anemia 4 (Hb <6.5)'))
+      enc.drug_induced_anaemia = 'Yes'
+    elsif (drug_cozed_symp == 'Lactic acidosis')
+      enc.drug_induced_lactic_acidosis = 'Yes'
+    else
+      enc.drug_induced_other_symptom = 'Yes'
+    end
   end
 
 end
@@ -476,14 +660,14 @@ end
 def pre_art_patient(patient_id, visit_day)
 
   received_before = $mysql_conn.query("SELECT * FROM tbpatientdrug WHERE FdxReferenceDrug NOT IN (56,148) AND
-                                        FdxReferencePatient = #{patient_id} AND FddBeginning < #{visit_day}")
+                                        FdxReferencePatient = #{patient_id} AND FddBeginning < '#{visit_day}'")
 
   if received_before.num_rows > 0
     return false
   else
-    receiving = $mysql_conn.query("SELECT * FROM tbfollowup AS e INNER JOIN tbfollowupdrug AS f ON e.FdxReference = f.FdxReferenceFollowUp
-                                    WHERE e.FddVisit < #{visit_day} AND e.FdxReferencePatient = #{patient_id} AND f.FdxReferenceDrug NOT IN
-                                    (56, 148)")
+    receiving = $mysql_conn.query("SELECT * FROM tbfollowup AS e INNER JOIN tbfollowupdrug AS f ON
+                                    e.FdxReference = f.FdxReferenceFollowUp WHERE e.FddVisit < '#{visit_day}'
+                                    AND e.FdxReferencePatient = #{patient_id} AND f.FdxReferenceDrug NOT IN (56, 148)")
     if receiving.num_rows > 0
       return false
     else
@@ -496,8 +680,8 @@ end
 
 def check_if_patient_has_tb(patientid, visit_date, tb_research)
 
-  check = $mysql_conn.query("SELECT * FROM tbfollowuptb WHERE FdxReferencePatient = #{patientid} AND DATE(#{visit_date})
-                            BETWEEN FddTreatmentFrom AND FddTreatmentTo")
+  check = $mysql_conn.query("SELECT * FROM tbfollowuptb WHERE FdxReferencePatient = #{patientid} AND DATE('#{visit_date}')
+                            BETWEEN DATE('FddTreatmentFrom') AND DATE('FddTreatmentTo')")
 
   if  check.num_rows > 0
       return "Confirmed TB on treatment"
@@ -509,6 +693,48 @@ def check_if_patient_has_tb(patientid, visit_date, tb_research)
      end
 
   end
+
+end
+
+def get_brought(amount_dispensed, adherence)
+  puts amount_dispensed
+  puts adherence
+
+  return amount_dispensed - (amount_dispensed *(adherence/100))
+
+end
+
+def get_dob(patient)
+
+  if patient[11].blank?
+     return get_dob_esitmated(patient[12],  patient[18])
+  else
+    return patient[11]
+  end
+
+end
+
+def amount_dispensed(dose,duration)
+
+  return dose * duration rescue duration
+
+end
+
+def create_patient_outcome(patientid, state, outcome_date)
+
+  visit_id =  self.check_for_visit_date(patientid,outcome_date)
+
+
+  pat_outcome = PatientOutcome.new()
+
+  pat_outcome.visit_encounter_id = visit_id
+  pat_outcome.outcome_id = $outcome_id
+  pat_outcome.patient_id = patientid
+  pat_outcome.outcome_state = state
+  pat_outcome.outcome_date = outcome_date
+  pat_outcome.save!
+  $outcome_id += 1
+
 
 end
 
